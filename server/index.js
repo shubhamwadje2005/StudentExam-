2d71c3ab8fbc6b2fe575bde6a7e504631f335e00
SHUBHAM WADJE <shubhamwadje2005@gmail.com>
2026-08-11 13:36:02 +0530

    initla setup

--- server/index.js
+++ server/index.js
@@ -10,7 +10,13 @@ const app = express()
 app.use(express.json())
 // app.use(express.static("dist"));
 // app.use(cors({ origin: "https://studentexam.onrender.com", credentials: true }))
-app.use(cors({ origin: "http://localhost:5173", credentials: true }))
+// app.use(cors({ origin: "http://localhost:5173", credentials: true }))
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "https://student-exam-client.vercel.app",
+    ], credentials: true
+}))
 app.use(cookieParser())
 
 app.use("/api/auth", require("./routes/auth.route"))
